5dcfc1ce5ad404893ad526a9cc6d4cfb16d7aa53
Sarera <Sarerabib@gmail.com>
2023-03-03 21:18:33 +0200

    loader/404

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes, useLocation} from "react-router-dom";
 import Home from "../pages/Home";
 import Schedule from "../pages/schedule";
 import Loader from "./Loader";
 
 const AppRouter = () => {
+
+    const location = useLocation();
+
     return (
         <Routes>
-            <Route path={'/'} element={<Home/>} />
-            <Route path={'/schedule'} element={<Schedule/>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/schedule" element={<Schedule />} />
             <Route path="*" element={<Loader/>} />
+            {location.pathname === '/404' && <Navigate to={location.state?.from || '/'} replace />}
         </Routes>
     );
 };
