6b18843d29483197b0aecb168deb776da65aa536
Sarera <Sarerabib@gmail.com>
2023-03-05 19:11:16 +0200

    domentime

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,21 +17,23 @@ const Home = () => {
 
             <div className={"d-flex m-3"}>
             <div id="list-example" className="list-group mx-3">
-                <a className    ="list-group-item list-group-item-action" href="src/pages#download">Установка</a>
-                <a className="list-group-item list-group-item-action" href="src/pages#create">Создание расписание</a>
-                <a className="list-group-item list-group-item-action" href="src/pages#upload">Загрузка расписание</a>
-                <a className="list-group-item list-group-item-action" href="src/pages#use-able">Пользывание ботом</a>
+
+                <a className    ="list-group-item list-group-item-action" href="#download">Установка</a>
+                <a className="list-group-item list-group-item-action" href="#create">Создание расписание</a>
+                <a className="list-group-item list-group-item-action" href="#upload">Загрузка расписание</a>
+                <a className="list-group-item list-group-item-action" href="#use-able">Пользывание ботом</a>
+
             </div>
             <div data-bs-spy="scroll" data-bs-target="#list-example" data-bs-offset="0" className="scrollspy-example m-2"
                  tabIndex="0">
                 <h4 id="download"> Как установить бота </h4>
-                <p>...</p>
+                <p>Text</p>
                 <h4 id="create">Как создать рассписание </h4>
-                <p>...</p>
+                <p>Text</p>
                 <h4 id="upload">Как загрузка рассписание</h4>
-                <p>...</p>
+                <p>Text</p>
                 <h4 id="use-able">Как пользывание ботом</h4>
-                <p>...</p>
+                <p>Text</p>
             </div>
             </div>
         </>
