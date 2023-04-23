b94b00672760980bf6e2584fbd96bc375c6731f7
Hordii <93381859+SareraBy@users.noreply.github.com>
2023-04-23 18:18:08 +0300

    info
    info

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -4,6 +4,7 @@ const Info = () => {
     return (
         <div className={"d-grid mt-5 text-center justify-content-center"} data-aos="flip-down">
             <h2 className={"text-center mb-5"}>Видео як користуватись</h2>
+
             <iframe
                 width="600"
                 height="325"
@@ -13,6 +14,7 @@ const Info = () => {
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
             ></iframe>
+
         </div>
     );
 };
