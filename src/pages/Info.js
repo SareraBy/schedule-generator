185e6e06c332f81e5d67eb9a385048ec9965bb93
Hordii <93381859+SareraBy@users.noreply.github.com>
2023-05-13 11:13:08 +0300

    Update Info.js
    Video

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -5,15 +5,18 @@ const Info = () => {
         <div className={"d-grid mt-5 text-center justify-content-center"} data-aos="flip-down">
             <h2 className={"text-center mb-5"}>Відео як користуватись</h2>
 
-            <iframe
-                width="600"
-                height="325"
-                src="https://www.youtube.com/embed/JFXHGVtggCY"
-                title="Як користуватись"
-                style={{borderRadius:"25px", border:"5px solid lightgreen"}}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-            ></iframe>
+             <div className="embed-responsive embed-responsive-16by9">
+                <iframe
+                    className="embed-responsive-item"
+                    src="https://www.youtube.com/embed/qIIojARxz40"
+                    title="Як користуватись"
+                    width={"300px"}
+                    height={"250px"}
+                    style={{borderRadius:"25px", border:"5px solid lightgreen"}}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                ></iframe>
+            </div>
 
         </div>
     );
